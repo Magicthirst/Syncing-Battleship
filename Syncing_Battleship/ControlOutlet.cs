a63cb8bc0d10f88cf620d0ab3b2ecd13d394e823
yoloroy <slart2108@gmail.com>
2025-07-17 11:48:04 +0300

    :construction: make accessible using docker

--- Syncing_Battleship/ControlOutlet.cs
+++ Syncing_Battleship/ControlOutlet.cs
@@ -1,14 +1,10 @@
 using System.Net;
-using Humanizer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Syncing_Battleship_gRPC_Outlet;
 using Syncing_Battleship_gRPC_Outlet.Services;
-using Util;
 
 namespace Syncing_Battleship;
 
@@ -19,7 +15,13 @@ public class ControlOutlet
     public ControlOutlet(ushort port, SessionsRouter sessions, Action stop)
     {
         var builder = WebApplication.CreateBuilder();
-        builder.WebHost.UseUrls($"http://localhost:{port}");
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.Listen(IPAddress.Any, port, listenOptions =>
+            {
+                listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+            });
+        });
 
         // TODO allow access only for Gateways url
 
@@ -41,7 +43,6 @@ public class ControlOutlet
         builder.Services.AddGrpc();
 
         app = builder.Build();
-        app.UseMiddleware<AdminSafeListMiddleware>("127.0.0.1;::1");
         app.MapGrpcService<SyncControlOutletImpl>();
     }
     
@@ -49,51 +50,4 @@ public class ControlOutlet
     public void Start() => _ = app.RunAsync();
 
     public void WaitForShutdown() => app.WaitForShutdown();
-
-    private class AdminSafeListMiddleware
-    {
-        private readonly RequestDelegate next;
-        private readonly ILogger<AdminSafeListMiddleware> logger;
-        private readonly byte[][] safelist;
-
-        public AdminSafeListMiddleware
-        (
-            RequestDelegate next,
-            ILogger<AdminSafeListMiddleware> logger,
-            string safelist
-        )
-        {
-            var ips = safelist.Split(';');
-            this.safelist = new byte[ips.Length][];
-            for (var i = 0; i < ips.Length; i++)
-            {
-                this.safelist[i] = IPAddress.Parse(ips[i]).GetAddressBytes();
-            }
-
-            this.next = next;
-            this.logger = logger;
-        }
-
-        public async Task Invoke(HttpContext context)
-        {
-            var remoteIp = context.Connection.RemoteIpAddress;
-            logger.LogDebug("Request from Remote IP address: {RemoteIp}", remoteIp);
-            if (remoteIp == null)
-            {
-                context.Response.StatusCode = (int) HttpStatusCode.Forbidden;
-                return;
-            }
-
-            var bytes = remoteIp.GetAddressBytes();
-            if (!safelist.Any(address => address.SequenceEqual(bytes)))
-            {
-                logger.LogWarning(
-                    "Forbidden Request from Remote IP address: {RemoteIp}", remoteIp);
-                context.Response.StatusCode = (int) HttpStatusCode.Forbidden;
-                return;
-            }
-
-            await next.Invoke(context);
-        }
-    }
 }
