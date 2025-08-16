3a501534c85c923952d55e2740e0443dbfb8839d
yoloroy <slart2108@gmail.com>
2025-08-16 12:37:00 +0300

    :sparkles: add movement hadling

--- Magicthrist - Green/MagicthirstDataBehaviour.cs
+++ Magicthrist - Green/MagicthirstDataBehaviour.cs
@@ -1,8 +1,9 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
 using Riptide;
+using Riptide.Utils;
 using Syncing_Battleship_Common_Typing;
 using static Magicthrist___Green.MagicthirstCommandMark;
+using static Magicthrist___Green.MagicthristGameState;
 
 namespace Magicthrist___Green;
 
@@ -10,32 +11,74 @@ public class MagicthirstDataBehaviour : IDataBehaviour
 {
     public object DefaultState => new MagicthristGameState();
 
-    public Message FullMessageOf(object state) => Message.Create();
+    public Message FullMessageOf(object state)
+    {
+        var src = (MagicthristGameState) state;
 
-    // public bool TryApplyNewPlayerConnection(int newPlayer, object state, out Message snapshot)
-    // {
-    //     throw new NotImplementedException();
-    // }
+        var message = Message.Create()
+            .AddInt(src.Players.Count);
+
+        foreach (var (id, player) in src.Players)
+        {
+            message
+                .AddInt(id)
+                .AddVector2(player.Position)
+                .AddVector2(player.Vector);
+        }
+
+        RiptideLogger.Log(LogType.Info, $"Full state: {src}");
+        return message;
+    }
+
+    public bool TryApplyNewPlayerConnection(int newPlayer, object state, out Message snapshot)
+    {
+        var src = (MagicthristGameState) state;
+        RiptideLogger.Log(LogType.Info, $"Adding new player to: {src}");
+        src.Players.Add(newPlayer, PlayerState.Default);
+        snapshot = Message.Create();
+        RiptideLogger.Log(LogType.Info, $"New player were added: {src}");
+        return false;
+    }
 
     // public bool TryApplyUpdate(Message message, int sender, object state, out Message snapshot)
     // {
     //     throw new NotImplementedException();
     // }
 
-    public bool TryApplyCommand(Message message, MessageMark mark, int sender, object state, out Message snapshot)
+    public bool TryApplyCommand
+    (
+        Message message,
+        MessageMark fullMark,
+        int sender,
+        object state,
+        out Message copy,
+        out Message snapshot
+    )
     {
+        var mark = fullMark & MessageMark.FilterExtras;
         var src = (MagicthristGameState) state;
         snapshot = message;
-        switch ((MagicthirstCommandMark)(mark & MessageMark.FilterExtras))
+        switch ((MagicthirstCommandMark) mark)
         {
             case Movement:
                 var player = src.Players[sender];
                 player.Position = message.GetVector2();
                 player.Vector = message.GetVector2();
+                var timestamp = message.GetDouble();
+
+                copy = Message.Create()
+                    .AddVector2(player.Position)
+                    .AddVector2(player.Vector)
+                    .AddDouble(timestamp)
+                    .AddInt(sender);
+
+                RiptideLogger.Log(LogType.Debug, $"Received movement position={player.Position}, vector={player.Vector}");
                 return false;
 
             default:
-                throw new ArgumentOutOfRangeException(nameof(mark));
+                copy = message;
+                RiptideLogger.Log(LogType.Warning, $"not handling command: {fullMark}");
+                return false;
         }
     }
 }
@@ -44,5 +87,5 @@ public class MagicthirstDataBehaviour : IDataBehaviour
 [SuppressMessage("ReSharper", "ShiftExpressionZeroLeftOperand")]
 internal enum MagicthirstCommandMark : ushort
 {
-    Movement = MessageMark.Command | 0 << MessageMarkSupply.ExtraShift
+    Movement = MessageMark.Command | 1 << MessageMarkSupply.ExtraShift
 }
--- Magicthrist - Green/MagicthristGameState.cs
+++ Magicthrist - Green/MagicthristGameState.cs
@@ -4,11 +4,19 @@ namespace Magicthrist___Green;
 
 internal class MagicthristGameState
 {
-    public Dictionary<int, PlayerState> Players;
+    public readonly Dictionary<int, PlayerState> Players = new();
 
     internal class PlayerState(Vector2 position, Vector2 vector)
     {
         public Vector2 Position = position;
         public Vector2 Vector = vector;
+
+        public static PlayerState Default => new(Vector2.Zero, Vector2.Zero);
     }
+
+    public override string ToString() =>
+        $"{nameof(MagicthristGameState)}(" +
+            $"Players=[{string.Join(",", Players)}]," +
+            $"|Players|={Players.Count}" +
+        $")";
 }
